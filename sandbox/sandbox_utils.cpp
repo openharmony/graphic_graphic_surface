46233ea4c05be6d1915318d470ba176680d0f7ba
sqwlly <shiqiwei4@huawei.com>
2024-03-05 01:54:00 +0000

    sync code Signed-off-by: s30029175 <shiqiwei4@huawei.com>
    Signed-off-by: sqwlly <shiqiwei4@huawei.com>
    Change-Id: Ie41f5d53646529a635d138eaec2759570243ea20

--- sandbox/sandbox_utils.cpp
+++ sandbox/sandbox_utils.cpp
@@ -22,63 +22,6 @@
 #endif
 
 namespace OHOS {
-#if !defined(OHOS_LITE) && !defined(_WIN32) && !defined(__APPLE__) && !defined(__gnu_linux__)
-static pid_t g_appSandboxPid_ = -1;
-
-const int PID_STR_SIZE = 5;
-const int STATUS_LINE_SIZE = 1024;
-const int PID_MAX_LEN = 11;
-
-static int FindAndConvertPid(char *buf, int len)
-{
-    int count = 0;
-    char pidBuf[PID_MAX_LEN] = {0};
-    char *str = buf;
-
-    if (len <= PID_STR_SIZE) {
-        return -1;
-    }
-
-    while (*str != '\0') {
-        if ((*str >= '0') && (*str <= '9') && (count < PID_MAX_LEN)) {
-            pidBuf[count] = *str;
-            count++;
-            str++;
-            continue;
-        }
-
-        if (count > 0) {
-            break;
-        }
-        str++;
-    }
-    return atoi(pidBuf);
-}
-
-static pid_t ParseRealPid(void)
-{
-    const char *path = "/proc/self/status";
-    char buf[STATUS_LINE_SIZE] = {0};
-    FILE *fp = fopen(path, "r");
-    if (fp == nullptr) {
-        return -1;
-    }
-
-    while (!feof(fp)) {
-        if (fgets(buf, STATUS_LINE_SIZE, fp) == nullptr) {
-            fclose(fp);
-            return -1;
-        }
-        if (strncmp(buf, "Tgid:", PID_STR_SIZE) == 0) {
-            break;
-        }
-    }
-    (void)fclose(fp);
-
-    return static_cast<pid_t>(FindAndConvertPid(buf, strlen(buf)));
-}
-#endif
-
 pid_t GetRealPid(void)
 {
 #ifdef _WIN32
@@ -86,10 +29,7 @@ pid_t GetRealPid(void)
 #elif defined(OHOS_LITE) || defined(__APPLE__) || defined(__gnu_linux__)
     return getpid();
 #else
-    if (g_appSandboxPid_ < 0) {
-        g_appSandboxPid_ = ParseRealPid();
-    }
-    return g_appSandboxPid_;
+    return getprocpid();
 #endif
 }
 } // namespace OHOS

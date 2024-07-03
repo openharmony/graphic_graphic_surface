58a2b98ad74fcbcd1970b9d33a790ea81a03df43
mengyiping <1091773127@qq.com>
2024-07-03 12:30:09 +0800

    --amend
    Signed-off-by: mengyiping <1091773127@qq.com>

--- sync_fence/src/acquire_fence_manager.cpp
+++ sync_fence/src/acquire_fence_manager.cpp
@@ -21,7 +21,7 @@ namespace OHOS {
 
 SyncFenceTracker* AcquireFenceTracker::tracker_ = new SyncFenceTracker("Acquire Fence");
 
-~AcquireFenceTracker(){
+AcquireFenceTracker::~AcquireFenceTracker(){
     if(tracker_ != nullptr){
         delete tracker_;
         tracker_ = nullptr;

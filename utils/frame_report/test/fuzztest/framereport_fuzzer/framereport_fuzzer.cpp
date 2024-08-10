fbf40aae6092d28c9452a6e392271da22f072472
JinKevin <920055454@qq.com>
2024-08-10 16:15:32 +0800

    IssueNo: #IAJ5IJ Description: 补充FrameReport类的Fuzz用例 Feature or Bugfix: Feature Binary Source: No Signed-off-by: 金凯文 <jinkaiwen2@huawei.com>

---
+++ utils/frame_report/test/fuzztest/framereport_fuzzer/framereport_fuzzer.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "framereport_fuzzer.h"
+
+#include <securec.h>
+
+#include "frame_report.h"
+
+namespace OHOS {
+namespace Rosen {
+namespace {
+const uint8_t* DATA = nullptr;
+size_t g_size = 0;
+size_t g_pos;
+} // namespace
+
+template<class T>
+T GetData()
+{
+    T object {};
+    size_t objectSize = sizeof(object);
+    if (DATA == nullptr || objectSize > g_size - g_pos) {
+        return object;
+    }
+    errno_t ret = memcpy_s(&object, objectSize, DATA + g_pos, objectSize);
+    if (ret != EOK) {
+        return {};
+    }
+    g_pos += objectSize;
+    return object;
+}
+
+bool DoSetGameScene(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+    // initialize
+    DATA = data;
+    g_size = size;
+    g_pos = 0;
+    // get data
+    int32_t pid = GetData<int32_t>();
+    int32_t state = GetData<int32_t>();
+    Rosen::FrameReport::GetInstance().SetGameScene(pid, state);
+    return true;
+}
+} // namespace Rosen
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::Rosen::DoSetGameScene(data, size);
+    return 0;
+}
+

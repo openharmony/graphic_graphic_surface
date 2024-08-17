99243cdd8eaa3c50478008d7d5f8a2f90fc24a27
mengyiping <1091773127@qq.com>
2024-08-17 16:53:01 +0800

    bugfix
    Signed-off-by: mengyiping <1091773127@qq.com>

---
+++ utils/rs_frame_report_ext/test/unittest/rs_frame_report_ext_test.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, Hardware
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "rs_frame_report_ext.h"
+
+namespace OHOS {
+using namespace testing;
+using namespace testing::ext;
+
+class RsFrameReportExtTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+};
+
+void RsFrameReportExtTest::SetUpTestCase() {}
+void RsFrameReportExtTest::TearDownTestCase() {}
+void RsFrameReportExtTest::SetUp() {}
+void RsFrameReportExtTest::TearDown() {}
+
+/**
+ * @tc.name: Init
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RsFrameReportExtTest, Init001, Function | MediumTest | Level2)
+{
+    RsFrameReportExt rsFrameReportExt_;
+    rsFrameReportExt_.Init();
+}
+
+/**
+ * @tc.name: GetEnable
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RsFrameReportExtTest, GetEnable001, Function | MediumTest | Level2)
+{
+    RsFrameReportExt rsFrameReportExt_;
+    rsFrameReportExt_.GetEnable();
+}
+
+/**
+ * @tc.name: HandleSwapBuffer
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RsFrameReportExtTest, HandleSwapBuffer001, Function | MediumTest | Level2)
+{
+    RsFrameReportExt rsFrameReportExt_;
+    rsFrameReportExt_.HandleSwapBuffer();
+}
+} // namespace OHOS

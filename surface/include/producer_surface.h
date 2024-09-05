03b167078382406183dd1b2dbcef41bcf28e8a6a
m00472246 <majingtao1@huawei.com>
2024-09-05 09:47:06 +0800

    解决disconnect和connect并发情况下，可能会出现连接状态异常的问题 Signed-off-by: m00472246 <majingtao1@huawei.com>
    Signed-off-by: m00472246 <majingtao1@huawei.com>

--- surface/include/producer_surface.h
+++ surface/include/producer_surface.h
@@ -219,10 +219,11 @@ public:
 private:
     bool IsRemote();
     void CleanAllLocked();
-    GSError AddCache(sptr<BufferExtraData> &bedataimpl,
+    GSError AddCacheLocked(sptr<BufferExtraData> &bedataimpl,
         IBufferProducer::RequestBufferReturnValue &retval, BufferRequestConfig &config);
     GSError SetMetadataValve(sptr<SurfaceBuffer>& buffer);
     void OutputRequestBufferLog(sptr<SurfaceBuffer>& buffer);
+    GSError CleanCacheLocked(bool cleanAll);
 
     mutable std::mutex mutex_;
     std::atomic_bool inited_ = false;
@@ -231,7 +232,7 @@ private:
     sptr<IBufferProducer> producer_ = nullptr;
     std::string name_ = "not init";
     uint64_t queueId_ = 0;
-    bool isDisconnected = true;
+    bool isDisconnected_ = true;
     sptr<IProducerListener> listener_;
     wptr<NativeWindow> wpNativeWindow_ = nullptr;
     wptr<ProducerSurfaceDelegator> wpPSurfaceDelegator_ = nullptr;

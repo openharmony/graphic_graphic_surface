1c2a3b3869efcc7e1a194ac8eb50f7b98208c407
蔡一鸣 <caiyiming4@huawei.com>
2025-01-24 03:06:20 +0000

    update surface/include/consumer_surface.h.
    Signed-off-by: 蔡一鸣 <caiyiming4@huawei.com>

--- surface/include/consumer_surface.h
+++ surface/include/consumer_surface.h
@@ -111,7 +111,6 @@ public:
     GSError DetachBufferFromQueue(sptr<SurfaceBuffer> buffer) override;
     GraphicTransformType GetTransformHint() const override;
     GSError SetTransformHint(GraphicTransformType transformHint) override;
-    GSError SetBufferName(const std::string &name) override;
     inline bool IsBufferHold() override
     {
         if (consumer_ == nullptr) {

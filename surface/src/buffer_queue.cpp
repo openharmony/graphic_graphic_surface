20ffdb80a33c04f3c21a7f7397764ee5b5df0cbc
zhangyifan <zhangyifan140@h-partners.com>
2026-07-27 03:11:18 +0000

    log level
    Signed-off-by: zhangyifan <zhangyifan140@h-partners.com>
    Change-Id: If2ea9bbddb0d7950866a2a21b196b86705cd2b35

--- surface/src/buffer_queue.cpp
+++ surface/src/buffer_queue.cpp
@@ -2376,7 +2376,7 @@ GSError BufferQueue::SetTunnelLayerInfo(const TunnelLayerInfo& info)
     }
     if (oldState.tunnelLayerId == newState.tunnelLayerId &&
         oldState.property == newState.property) {
-        BLOGW("same tunnel layer info, uniqueId: %{public}" PRIu64 ".", uniqueId_);
+        BLOGD("same tunnel layer info, uniqueId: %{public}" PRIu64 ".", uniqueId_);
         return GSERROR_OK;
     }
     sptr<IBufferConsumerListener> listener;
